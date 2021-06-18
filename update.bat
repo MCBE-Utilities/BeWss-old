275d4b5767bcc0ab659ace01d8209e86af08796d
PMK744 <69542280+PMK744@users.noreply.github.com>
2021-06-18 12:50:34 -0500

    Upated update.bat

---
+++ update.bat
@@ -0,0 +1,11 @@
+@echo off
+cd .
+
+echo.
+where npm >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Error: BeWss requires npm. Please install node.js first 1>&2
+    exit /b %errorlevel%
+) 
+echo ^> npm i
+call npm i
