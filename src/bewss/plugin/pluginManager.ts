ad95fb01f2a846d2174037fa0995901858a8f994
Nobu <nowobuwu@gmail.com>
2021-06-21 22:24:05 -0500

    new plugin manager

--- src/bewss/plugin/pluginManager.ts
+++ src/bewss/plugin/pluginManager.ts
@@ -1,7 +1,8 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import chalk from 'chalk'
 import moment from 'moment'
 import bewss from "../bewss"
-import path from "path"
+import path, { resolve } from "path"
 import fs from "fs"
 import fse from "fs-extra"
 import childProcess from "child_process"
@@ -36,98 +37,232 @@ interface examplePluginConfig {
 }
 class pluginManager {
   private bewss: bewss
-  private plugins = new Map<string, { config: examplePluginConfig, plugin: examplePlugin }>()
+  private plugins = new Map<string, { config: examplePluginConfig, plugin: examplePlugin }>() // path: { config, plugin }
+  public root = path.resolve(process.cwd())
+  public pluginsPath = path.resolve(this.root, '../plugins')
 
   constructor(bewss: bewss) {
     this.bewss = bewss
   }
 
   async onEnabled(): Promise<void> {
-    //
-  }
-
-  async onDisabled(): Promise<void> {
-<<<<<<< HEAD
-    //
-=======
-    for (const plugin of this.plugins.values()) {
-      plugin.onDisabled()
+    await this.loadAll()
+    for (const [path, { config, plugin }] of this.plugins.entries()) {
+      try {
+        plugin.onEnabled()
+      } catch (error) {
+        this.error(`Plugin "${config.name || path}". Uncaught Exception(s):\n`, error)
+      }
     }
 
     return
   }
 
-  async loadPlugins(): Promise<void> {
-    return new Promise(async (res) => {
-      const folders: Array<string> = []
+  async onDisabled(): Promise<void> {
+    for (const [path, { config, plugin }] of this.plugins.entries()) {
+      try {
+        plugin.onDisabled()
+      } catch (error) {
+        this.error(`Plugin "${config.name || path}". Uncaught Exception(s):\n`, error)
+      }
+    }
 
-      const pluginFolderPath = path.resolve(process.cwd(), `../plugins`)
-      if (!fs.existsSync(pluginFolderPath)) {
-        this.warn("Plugins dir not found skipping plugin manager. To use plugins please create a dir named \"plugins\" in this location:", '"' + path.resolve(process.cwd(), `../`) + '"')
+    return
+  }
+
+  async loadAll(): Promise<void> {
+    return new Promise(async (res) => {
+      if (!fs.existsSync(this.pluginsPath)) {
+        this.warn("ENOENT: Plugins folder does not exist. Creating plugins folder:", `"${this.pluginsPath}"`)
+        fs.mkdirSync(this.pluginsPath, { recursive: true })
+
+        return res()
+      }
+
+      const pluginsDirs: string[] = []
+      for (const item of fs.readdirSync(this.pluginsPath)) {
+        if (!fs.statSync(path.resolve(this.pluginsPath, item)).isDirectory()) continue
+        pluginsDirs.push(item)
+      }
+
+      if (pluginsDirs.length < 1) {
+        this.info('No plugins found!')
         
         return res()
       }
-      const pluginFolder = fs.readdirSync(pluginFolderPath)
 
-      for (const file of pluginFolder) {
-        if (!fs.statSync(path.resolve(process.cwd(), `../plugins/${file}`)).isDirectory()) continue
-        folders.push(file)
-      }
-  
-      if (folders.length < 1) {
-        this.info('No plugins found!')
-        res()
+      for (const plugin of pluginsDirs) {
+        await this.register(resolve(this.pluginsPath, plugin))
       }
 
-      for (const folder of folders) {
-        try {
-          const pluginPath = path.resolve(process.cwd(), `../plugins/${folder}`)
-          if (!fs.existsSync(path.resolve(pluginPath, 'package.json'))) return this.error(`plugins/${folder} missing package.json. Cannot load plugin!`)
-          const config: examplePluginConfig = require(path.resolve(pluginPath, 'package.json'))
-          const mainEntry = path.resolve(pluginPath, config.main)
-          fse.copySync(path.resolve(process.cwd(), 'src/bewss/@interface'), path.resolve(`${pluginPath}/src/@interface`))
-          let neededUpdate = false
-          let succeededUpdate = true
-          if (!fs.existsSync(path.resolve(pluginPath, "node_modules"))) {
-            neededUpdate = true
-            succeededUpdate = await this.updatePlugin(pluginPath, config)
-          }
-          if (neededUpdate && !succeededUpdate) return this.warn(`Skipping plugin "${config.name}" due to errors during installing dependencies`)
-          let buildSuc = true
-          let alrbuilt = false
-          if (!fs.existsSync(path.resolve(pluginPath, "dist"))) {
-            const resp = await this.buildPlugin(pluginPath, config)
-            buildSuc = resp
-            alrbuilt = resp
-          }
-          if (!config.devMode === false && !alrbuilt) {
-            this.warn(`[${config.name}] devMode enabled, set devMode to false in package.json to disable`)
-            buildSuc = await this.buildPlugin(pluginPath, config)
-          }
-          if (buildSuc) {
-            try {
-              const PluginClass = require(mainEntry)
-              const newPlugin = new PluginClass(this.bewss)
-              this.info(`Successfully loaded ${config.name}!`)
-              this.plugins.set(config.name, newPlugin)
-            } catch (error) {
-              this.error(`"${config.name}"` + "Uncaught Exception(s):\n", error)
-            }
-          } else {
-            this.warn(`Skipping plugin "${config.name}" due to errors during building`)
-          }
-        } catch (error) {
-          this.error("Failed to load plugin " + `plugins/${folder}, ` + "Recieved Error(s):\n", error)
-        }
-      }
-
-      res()
+      return res()
     })
->>>>>>> 94cdbed25af3c8450102be8da41aeaada28362a5
   }
 
-  async register(): Promise<void> {
-    
+  async register(path: string): Promise<void> {
+    return new Promise(async (res) => {
+      try {
+        const confPath = resolve(path, "package.json")
+        if (!fs.existsSync(confPath)) {
+          this.error(`ENOENT: package.json does not exist in "${path}". Skipping plugin!`)
+
+          return res()
+        }
+        const config: examplePluginConfig = await import(confPath)
+        if (!this.verifyConfig(path, config)) return res()
+        const entryPoint = resolve(path, config.main)
+        fse.copySync(resolve(process.cwd(), 'src/bewss/@interface'), resolve(path, "src", "@interface"))
+
+        let neededUpdate = false
+        let succeededUpdate = false
+        let buildSuccess = true
+        let alreadyBuilt = false
+
+        if (!fs.existsSync(resolve(path, "node_modules")) && (config.dependencies || config.devdependencies)) {
+          neededUpdate = true
+          succeededUpdate = await this.update(path, config)
+        }
+        if (neededUpdate && !succeededUpdate) {
+          this.error(`Skipping plugin "${config.name || path}". Failed to install dependencies.`)
+          
+          return res()
+        }
+        if (!fs.existsSync(resolve(path, "dist"))) {
+          buildSuccess = await this.build(path, config)
+          alreadyBuilt = true
+        }
+        if (config.devMode !== false && !alreadyBuilt) {
+          this.warn(`Plugin "${config.name || path}" is in dev mode. Set devMode to false in package.json to disable`)
+          buildSuccess = await this.build(path, config)
+        }
+        if (!buildSuccess) {
+          this.error(`Skipping plugin "${config.name || path}". Failed to build.`)
+
+          return res()
+        }
+        try {
+          const plugin: examplePlugin = require(entryPoint)
+          const newPlugin = new plugin(this.bewss)
+          this.info(`Successfully loaded plugin "${config.name || path}"`)
+          this.plugins.set(path, {
+            config,
+            plugin: newPlugin, 
+          })
+        } catch (error) {
+          this.error(`Plugin "${config.name || path}". Uncaught Exception(s):\n`, error)
+        }
+
+      } catch (error) {
+        this.error(`Failed to load plugin "${path}". Recieved Error(s):\n`, error)
+      }
+
+      return res()
+    })
+  }
+  // path or name
+  unregister(name: string): string | undefined {
+    const plugin = this.plugins.get(name) || Array.from(this.plugins.values()).find(({ config }) => config.name === name)
+
+    if (!plugin) return
+    const path: string = this.getByValue(this.plugins, plugin)
+    if (!path) return
+    delete require.cache[require.resolve(resolve(path))]
+    delete require.cache[require.resolve(resolve(path, "package.json"))]
+    delete require.cache[require.resolve(resolve(path, plugin.config.main))]
+
+    this.plugins.delete(path)
+
+    return path
+  }
+  
+  // name || path
+  async reload(name: string): Promise<void>{
+    const unregister = this.unregister(name)
+    if (unregister) {
+      await this.register(unregister)
+
+      return Promise.resolve()
+    }
+  }
+
+  async reloadAll(): Promise<void> {
+    const plugins = this.plugins.keys()
+    for (const plugin of plugins) {
+      await this.reload(plugin)
+    }
+
+    return Promise.resolve()
+  }
+
+  getByValue(map: Map<unknown, unknown>, searchValue: any): any {
+    for (const [k, v] of map.entries()) {
+      if (v === searchValue)
+      return k
+    }
+  }
+
+  private async update(path: string, config: examplePluginConfig): Promise<boolean> {
+    return new Promise((res) => {
+      this.info(`Installing dependencies for "${config.name || path}"`)
+      childProcess.exec('npm i', {
+        cwd: path,
+      }, (err, out) => {
+        if (err) {
+          this.error(`Failed to install dependencies for "${config.name || path}". Recieved Error(s):\n`, out)
+          res(false)
+        } else {
+          this.info(`Finished installing dependencies for "${config.name || path}"`)
+          res(true)
+        }
+      })
+    })
+  }
+  private async build(path: string, config: examplePluginConfig): Promise<boolean> {
+    return new Promise((res) => {
+      this.info(`Attempting to build "${config.name || path}"`)
+      childProcess.exec('npm run build', {
+        cwd: path,
+      }, (err, out) => {
+        if (err) {
+          this.error(`Failed to build "${config.name || path}". Recieved Error(s):\n`, out)
+          res(false)
+        } else {
+          this.info(`Successfully built "${config.name || path}"`)
+          res(true)
+        }
+      })
+    })
+  }
+
+  verifyConfig(path: string, config: examplePluginConfig): boolean {
+    if (!config.name) this.warn(`plugin "${path}" missing name prop in package.json. Will be refered to as "${path}"`)
+    if (!config.version) this.warn(`plugin "${config.name || path}" missing version prop in package.json`)
+    if (config.devMode === undefined) this.info(`plugin "${config.name || path}" missing devMode prop in package.json. Auto defaults to true!`)
+    if (!config.main) {
+      this.error(`plugin "${config.name || path}" missing main prop in package.json. Cannot start plugin without valid path to main file!`)
+      
+      return false
+    }
+    if (!config.scripts) {
+      this.error(`plugin "${config.name || path}" missing scripts in package.json. Cannot start plugin without needed scripts!`)
+
+      return false
+    }
+    if (!config.scripts.build) {
+      this.error(`plugin "${config.name || path}" missing scripts.build in package.json. Cannot start plugin without needed scripts!`)
+
+      return false
+    }
+    if (!config.scripts.start) {
+      this.error(`plugin "${config.name || path}" missing main scripts.start in package.json. Cannot start plugin without needed scripts!`)
+
+      return false
+    }
+    if (config.dependencies && Object.keys(config.dependencies).filter(i => i !== "ts-node" && i !== "typescript").length === 0) {
+      this.info(`Congrats @${config.author || config.name || path}, your plugin does not use any dependencies other than the needed ones!`)
+    } 
+
+    return true
   }
 
 
