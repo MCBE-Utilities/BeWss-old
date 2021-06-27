b0cb6115c87e8f1bbdd0ef1686650d7987519fcd
Nobu <nowobuwu@gmail.com>
2021-06-26 21:09:07 -0500

    port fallback

--- src/bewss/server/serverManager.ts
+++ src/bewss/server/serverManager.ts
@@ -28,29 +28,28 @@ class serverManager {
 
   private async createServer(): Promise<void> {
     return new Promise((res) => {
-      try {
-        this.ws = new Server({ port: this.port })
+      this.ws = new Server({ port: this.port })
 
-        this.ws.on('listening', () => {
-          this.bewss.getEventManager().emit('wss-listening')
-          this.bewss.getLogger().info(`Websocket server started! To connect to your server do "/connect 127.0.0.1:${this.port}" in your Minecraft world.`)
-        })
-        this.ws.on('connection', (wss: Server) => {
-          this.server = wss
-          this.server.setMaxListeners(50)
-          this.bewss.getEventManager().emit('wss-connected')
-          this.bewss.getLogger().info('User connected to the server!')
-          this.bewss.getLogger().info('Do -help for a list of BeWss commands, do /help for a list of Bedrock commands.')
-        })
-      } catch (error) {
+      this.ws.once('listening', () => {
+        this.bewss.getEventManager().emit('wss-listening')
+        this.bewss.getLogger().info(`Websocket server started! To connect to your server do "/connect 127.0.0.1:${this.port}" in your Minecraft world.`)
+      })
+      this.ws.once('connection', (wss: Server) => {
+        this.server = wss
+        this.server.setMaxListeners(50)
+        this.bewss.getEventManager().emit('wss-connected')
+        this.bewss.getLogger().info('User connected to the server!')
+        this.bewss.getLogger().info('Do -help for a list of BeWss commands, do /help for a list of Bedrock commands.')
+      })
+      this.ws.once('error', () => {
         this.bewss.getLogger().error("Failed to open ws server on port", this.port + ".", "Incrementing port by 1 and attempting again in 5.00s")
         setTimeout(() => {
           this.port++
           this.createServer()
         }, 5000)
-      }
 
-      res()
+        res()
+      })
     })
   }
 
