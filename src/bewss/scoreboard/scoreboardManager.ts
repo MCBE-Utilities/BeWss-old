3ce560567ce18e8809e16af4a2fd0eba050d8770
PMK744 <69542280+PMK744@users.noreply.github.com>
2021-06-26 19:42:33 -0500

    Updated scoreboardManager to new response system

--- src/bewss/scoreboard/scoreboardManager.ts
+++ src/bewss/scoreboard/scoreboardManager.ts
@@ -19,107 +19,79 @@ class scoreboardManager {
   }
 
   async createObjective(objective: string, displayname?: string): Promise<genericScoreboard> {
-    return new Promise((res) => {
-      const command = this.bewss.getCommandManager().executeCommand(`/scoreboard objectives add ${objective} dummy "${displayname}"`) as commandResponse
-      this.bewss.getCommandManager().on('ScoreboardObjectiveCommandExecuted', (packet: genericScoreboard) => {
-        if (command == undefined || command.requestId != packet.header.requestId) return res(undefined)
-  
-        return res(packet)
-      })
-    })
+    const command = this.bewss.getCommandManager().executeCommand(`/scoreboard objectives add ${objective} dummy "${displayname}"`) as commandResponse
+    const response = await this.bewss.getCommandManager().findResponse(command.requestId) as genericScoreboard
+    
+    return response
   }
 
   async removeObject(objective: string): Promise<genericScoreboard> {
-    return new Promise((res) => {
-      const command = this.bewss.getCommandManager().executeCommand(`/scoreboard objectives remove ${objective}`) as commandResponse
-      this.bewss.getCommandManager().on('ScoreboardObjectiveCommandExecuted', (packet: genericScoreboard) => {
-        if (command == undefined || command.requestId != packet.header.requestId) return res(undefined)
+    const command = this.bewss.getCommandManager().executeCommand(`/scoreboard objectives remove ${objective}`) as commandResponse
+    const response = await this.bewss.getCommandManager().findResponse(command.requestId) as genericScoreboard
   
-        return res(packet)
-      })
-    })
+    return response
   }
 
   async setdisplay(objective: string, display: displays, layout?: layouts | ""): Promise<genericScoreboard> {
-    return new Promise((res) => {
-      if (layout == undefined) layout = ""
-      const command = this.bewss.getCommandManager().executeCommand(`/scoreboard objectives setdisplay ${display} ${objective} ${layout}`) as commandResponse
-      this.bewss.getCommandManager().on('ScoreboardObjectiveCommandExecuted', (packet: genericScoreboard) => {
-        if (command == undefined || command.requestId != packet.header.requestId) return res(undefined)
-  
-        return res(packet)
-      })
-    })
+    const command = this.bewss.getCommandManager().executeCommand(`/scoreboard objectives setdisplay ${display} ${objective} ${layout}`) as commandResponse
+    const response = await this.bewss.getCommandManager().findResponse(command.requestId) as genericScoreboard
+
+    return response
   }
 
   async getObjectives(): Promise<getObjectives> {
-    return new Promise(async (res) => {
-      const command = this.bewss.getCommandManager().executeCommand(`/scoreboard objectives list`) as commandResponse
-      this.bewss.getCommandManager().on('ScoreboardObjectiveCommandExecuted', async (packet: getObjectives) => {
-        if (command == undefined || command.requestId != packet.header.requestId) return res(undefined)
-        const raw: Array<string> = packet.body.statusMessage.split('\n')
-        const objectives: Array<Array<string>> = []
-        for await (const string of raw) {
-          if (!string.startsWith("§a")) {
-            const rawString: Array<string> = string.split(' ')
-            objectives.push([rawString[1].replace(':', ''), string.replace(`- ${rawString[1].replace(':', '')}: displays as '`, '').replace(`' and is type 'dummy'`, '')])
-          }
-        }
-        packet.body.objectives = objectives
+    const command = this.bewss.getCommandManager().executeCommand(`/scoreboard objectives list`) as commandResponse
+    const response: getObjectives = await this.bewss.getCommandManager().findResponse(command.requestId) as getObjectives
+    const raw: Array<string> = response.body.statusMessage.split('\n')
+    const objectives: Array<Array<string>> = []
+    for await (const string of raw) {
+      if (!string.startsWith("§a")) {
+        const rawString: Array<string> = string.split(' ')
+        objectives.push([rawString[1].replace(':', ''), string.replace(`- ${rawString[1].replace(':', '')}: displays as '`, '').replace(`' and is type 'dummy'`, '')])
+      }
+    }
+    response.body.objectives = objectives
 
-        return res(packet)
-      })
-    })
+    return response
   }
 
   async getObjectiveName(objective: string): Promise<string> {
-    return new Promise(async (res) => {
-      let displayname: string
-      const objectives = await this.getObjectives()
-      if (objectives == undefined) return
-      for (const obj of objectives.body.objectives) {
-        if (obj[0] == objective) {
-          displayname = obj[1]
-        }
+    let displayname: string
+    const objectives = await this.getObjectives()
+    if (objectives == undefined) return
+    for (const obj of objectives.body.objectives) {
+      if (obj[0] == objective) {
+        displayname = obj[1]
       }
+    }
 
-      return res(displayname)
-    })
+    return displayname
   }
 
   async updateScore(target: string, operation: operations, objective: string, amount: number): Promise<genericScoreboard> {
-    return new Promise((res) => {
-      const command = this.bewss.getCommandManager().executeCommand(`/scoreboard players ${operation} "${target}" ${objective} ${amount}`) as commandResponse
-      this.bewss.getCommandManager().on('ScoreboardPlayerCommandExecuted', (packet: genericScoreboard) => {
-        if (command == undefined || command.requestId != packet.header.requestId) return res(undefined)
-  
-        return res(packet)
-      })
-    })
+    const command = this.bewss.getCommandManager().executeCommand(`/scoreboard players ${operation} "${target}" ${objective} ${amount}`) as commandResponse
+    const response = await this.bewss.getCommandManager().findResponse(command.requestId) as genericScoreboard
+
+    return response
   }
 
   async getScore(target: string, objective: string): Promise<getScore> {
-    return new Promise((res) => {
-      const command = this.bewss.getCommandManager().executeCommand(`/scoreboard players list "${target}"`) as commandResponse
-      this.bewss.getCommandManager().on('ScoreboardPlayerCommandExecuted', async (packet: getScore) => {
-        if (command == undefined || command.requestId != packet.header.requestId) return res(undefined)
-        const objectiveName = await this.getObjectiveName(objective)
-        let score: number
-        const raw: Array<string> = packet.body.statusMessage.split('\n')
-        for (const string of raw) {
-          if (!string.startsWith("§a") && string.includes(objectiveName)) {
-            const rawString: Array<string> = string.split(' ').reverse()
-            score = parseInt(rawString[1])
-          }
-        }
-        packet.body.score = score
-        packet.body.objective = objective
-        packet.body.objectiveName = objectiveName
-        console.log(`Objective: ${objective} Score: ${score}`)
+    const command = this.bewss.getCommandManager().executeCommand(`/scoreboard players list "${target}"`) as commandResponse
+    const response = await this.bewss.getCommandManager().findResponse(command.requestId) as getScore
+    const objectiveName = await this.getObjectiveName(objective)
+    let score: number
+    const raw: Array<string> = response.body.statusMessage.split('\n')
+    for (const string of raw) {
+      if (!string.startsWith("§a") && string.includes(objectiveName)) {
+        const rawString: Array<string> = string.split(' ').reverse()
+        score = parseInt(rawString[1])
+      }
+    }
+    response.body.score = score
+    response.body.objective = objective
+    response.body.objectiveName = objectiveName
 
-        return res(packet)
-      })
-    })
+    return response
   }
 
 }
